9846083b05163a960b3d5e565b7bc9c53a4bcd22
Pauli Simonen <pauli@lanttu.fi>
2024-10-25 17:06:15 +0300

    Initial commit

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# https://typeof.net/Iosevka/customizer
+
+if [[ ! $(docker images --quiet fontcc) ]]; then
+	git clone --depth=1 https://github.com/be5invis/Iosevka.git fontcc
+
+	(cd fontcc && docker build --tag fontcc:latest .)
+
+	rm --force --recursive fontcc
+fi
+
+docker run --interactive --name fontcc --rm --tty --user $(id -u):$(id -g) --volume $(pwd):/work fontcc ttf::Iosevka
+
+rsync --delete-after --recursive dist/Iosevka/TTF/ ~/.local/share/fonts/Iosevka/
+
+rm --force --recursive dist
+
+fc-cache --force
